206a0391306a92e543f7976de3a0e473005919e3
SelimHorri <horri.slim@gmail.com>
2021-03-27 23:19:11 +0100

    add some practical examples

--- case-logic.sql
+++ case-logic.sql
@@ -60,4 +60,46 @@ FROM dual;
 
 
 
+--practical example
+select 
+    case
+        --select from beginning of string(0) to '/'
+        when length(NVL(SUBSTR(label, 0, INSTR(label, '/')-1), label)) <=11 then NVL(SUBSTR(label, 0, INSTR(label, '/')-1), label)
+        --select from '/' to end of string
+        when length(NVL(SUBSTR(label, 0, INSTR(label, '/')-1), label)) >11 then NVL(SUBSTR(label, INSTR(label, '/')+1, length(label)), label)
+        else NULL
+      end as test_var,
+      date,
+      groupname,
+      label
+from testdata
+where length(NVL(SUBSTR(label, 0, INSTR(label, '/')-1), label))>11;
+
+
+
+
+ select * from (
+   SELECT date,
+   total,
+   groupname, NVL(SUBSTR(label, 0, INSTR(label, '/')-1), label) as label, rank() over ( partition by groupname, date order by total desc) ranking
+   FROM (
+     WITH date_time as (
+       select trunc(add_months(sysdate, -3
+       ), 'mm') startdate,
+       trunc(add_months(sysdate, 0), 'mm') as enddate
+     )
+     SELECT to_char(trunc(date, 'MM'), 'MM/YYYY') date, round(sum(kpi1 + kpi2 + kpi3 + kpi4)/1000000,5) as total,
+     groupname, NVL(SUBSTR(label, 0, INSTR(label, '/')-1), label) as label
+     FROM testdata, date_time
+    WHERE  DATE >= startdate  AND DATE < enddate
+     group by trunc(date, 'MM'), groupname, NVL(SUBSTR(label, 0, INSTR(label, '/')-1), label)
+   )
+ )
+ where ranking IN (1,2,3,4,5) and total !=0
+ order by  date desc, groupname, ranking;
+ 
+
+
+
+
 
